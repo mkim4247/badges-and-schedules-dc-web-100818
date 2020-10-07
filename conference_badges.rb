059c3966377b88e8e1720ee565ba21b5d0053eda
Michael Kim <m.kim4247@gmail.com>
2020-10-07 17:19:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,2 +1,14 @@
 # Write your code here.
 
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names_batch)
+  names_batch.each do |name|
+    "Hello, my name is #{name}."
+end
+
+def assign_rooms
+  
+end
